16e7e8a4081496f90743f09131dea9492bdf3c01
Luiz Felipe <73030375+LFelipe7@users.noreply.github.com>
2023-09-26 18:05:55 -0300

    🎉 Movies Cataloguewith Svelte

---
+++ catalogofilmes/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script context='module'>   
+export async function load({ fetch }) {
+    const rest = await fetch(
+        'https://api.themoviedb.org/3/movie/popular?api_key=f0766485c60cf9fcc2f8dbef9b435ecc'
+    );
+    const data = await rest.json();
+    if(rest.ok){
+        return {
+            props: {popular: data.results}
+        }
+    }
+}
+</script>
+
+<script>
+    import popularMovies from "./components/popularMovies.svelte"
+    export let popular;
+</script>
+
+<section>
+    <popularMovies {popular} />
+</section>
---
+++ catalogofilmes/src/routes/components/popularMovies.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let popular;
+    console.log(popular[0]);
+</script>
+
+<h1>Popular Movies</h1>
+<div class="popular-movies">
+    <div class="movie">
+
+    </div>
+</div>
